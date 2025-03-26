58678992e79734bf0edaecdacf43116d259b462b
Bui Thanh Van <van.bt@evotek.vn>
2025-03-26 22:32:46 +0700

    day1

--- js/script.js
+++ js/script.js
@@ -1,64 +1,92 @@
+var love = setInterval(function () {
+  var r_num = Math.floor(Math.random() * 40) + 1;
+  var r_size = Math.floor(Math.random() * 65) + 10;
+  var r_left = Math.floor(Math.random() * 100) + 1;
+  var r_bg = Math.floor(Math.random() * 25) + 100;
+  var r_time = Math.floor(Math.random() * 5) + 5;
 
-      var love = setInterval(function () {
-        var r_num = Math.floor(Math.random() * 40) + 1;
-        var r_size = Math.floor(Math.random() * 65) + 10;
-        var r_left = Math.floor(Math.random() * 100) + 1;
-        var r_bg = Math.floor(Math.random() * 25) + 100;
-        var r_time = Math.floor(Math.random() * 5) + 5;
+  $(".bg_heart").append(
+    "<div class='heart' style='width:" +
+      r_size +
+      "px;height:" +
+      r_size +
+      "px;left:" +
+      r_left +
+      "%;background:rgba(255," +
+      (r_bg - 25) +
+      "," +
+      r_bg +
+      ",1);-webkit-animation:love " +
+      r_time +
+      "s ease;-moz-animation:love " +
+      r_time +
+      "s ease;-ms-animation:love " +
+      r_time +
+      "s ease;animation:love " +
+      r_time +
+      "s ease'></div>"
+  );
 
-        $(".bg_heart").append(
-          "<div class='heart' style='width:" +
-            r_size +
-            "px;height:" +
-            r_size +
-            "px;left:" +
-            r_left +
-            "%;background:rgba(255," +
-            (r_bg - 25) +
-            "," +
-            r_bg +
-            ",1);-webkit-animation:love " +
-            r_time +
-            "s ease;-moz-animation:love " +
-            r_time +
-            "s ease;-ms-animation:love " +
-            r_time +
-            "s ease;animation:love " +
-            r_time +
-            "s ease'></div>"
-        );
+  $(".bg_heart").append(
+    "<div class='heart' style='width:" +
+      (r_size - 10) +
+      "px;height:" +
+      (r_size - 10) +
+      "px;left:" +
+      (r_left + r_num) +
+      "%;background:rgba(255," +
+      (r_bg - 25) +
+      "," +
+      (r_bg + 25) +
+      ",1);-webkit-animation:love " +
+      (r_time + 5) +
+      "s ease;-moz-animation:love " +
+      (r_time + 5) +
+      "s ease;-ms-animation:love " +
+      (r_time + 5) +
+      "s ease;animation:love " +
+      (r_time + 5) +
+      "s ease'></div>"
+  );
 
-        $(".bg_heart").append(
-          "<div class='heart' style='width:" +
-            (r_size - 10) +
-            "px;height:" +
-            (r_size - 10) +
-            "px;left:" +
-            (r_left + r_num) +
-            "%;background:rgba(255," +
-            (r_bg - 25) +
-            "," +
-            (r_bg + 25) +
-            ",1);-webkit-animation:love " +
-            (r_time + 5) +
-            "s ease;-moz-animation:love " +
-            (r_time + 5) +
-            "s ease;-ms-animation:love " +
-            (r_time + 5) +
-            "s ease;animation:love " +
-            (r_time + 5) +
-            "s ease'></div>"
-        );
+  $(".heart").each(function () {
+    var top = $(this)
+      .css("top")
+      .replace(/[^-\d\.]/g, "");
+    var width = $(this)
+      .css("width")
+      .replace(/[^-\d\.]/g, "");
+    if (top <= -100 || width >= 150) {
+      $(this).detach();
+    }
+  });
+}, 500);
 
-        $(".heart").each(function () {
-          var top = $(this)
-            .css("top")
-            .replace(/[^-\d\.]/g, "");
-          var width = $(this)
-            .css("width")
-            .replace(/[^-\d\.]/g, "");
-          if (top <= -100 || width >= 150) {
-            $(this).detach();
+$(window).on("load", function () {
+  function startCountdown(targetDate) {
+      function updateClock() {
+          const now = new Date().getTime();
+          const distance = now - targetDate;
+          console.log(distance);
+          
+          if (distance < 0) {
+              $("#clock date").text("EXPIRED");
+              $("#clock time").text("00:00:00");
+              return;
           }
-        });
-      }, 500);
+
+          const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+          const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+          const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+          $("#clock date").text(`${days} DAYS`);
+          $("#clock time").text(`${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`);
+      }
+
+      updateClock();
+      setInterval(updateClock, 1000);
+  }
+
+  startCountdown(new Date("2024-11-15T00:00:00").getTime());
+});
